f597322c6efe804896e7a004dabf916e70629633
Pierre Morvant <PierreKiwi@users.noreply.github.com>
2016-10-18 15:14:00 +1300

    Add support for new Region Ohio us-east-2

--- src/main/java/com/amazonaws/codepipeline/jenkinsplugin/AWSCodePipelineSCM.java
+++ src/main/java/com/amazonaws/codepipeline/jenkinsplugin/AWSCodePipelineSCM.java
@@ -59,7 +59,8 @@ public class AWSCodePipelineSCM extends hudson.scm.SCM {
     public static final Regions[] AVAILABLE_REGIONS = {
         Regions.EU_WEST_1,
         Regions.US_EAST_1,
-        Regions.US_WEST_2
+        Regions.US_WEST_2,
+        Regions.US_EAST_1
     };
 
     public static final CategoryType[] ACTION_TYPE = {
